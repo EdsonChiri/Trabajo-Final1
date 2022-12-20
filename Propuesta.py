9958157e7d0ce69c680b3aedba130e3dfd696328
EdsonChiri <echirigo@unsa.edu.pe>
2022-12-20 00:48:35 -0500

    tarea

---
+++ Propuesta.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu("Menu", ["Home", 'Base teórica','Propuesta','Resultados','Conclusiones'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+
+
+
+
+  
+
+
